6dbb7b0b51ae19e66a147fa456c0ec85b7d53f58
Asif Iqbal <50192401+iqbca@users.noreply.github.com>
2025-12-25 23:40:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+        entry.target.style.transition = "all 0.7s ease";
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+reveals.forEach(r => observer.observe(r));
